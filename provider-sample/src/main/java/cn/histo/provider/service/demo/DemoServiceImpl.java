bbdbde669a205cc72d735bbc1649cffc738e6c3b
Wildmanli <wildmanli@hotmail.com>
2021-08-30 10:09:22 +0800

    添加配置中心动态刷新示例

--- provider-sample/src/main/java/cn/histo/provider/service/demo/DemoServiceImpl.java
+++ provider-sample/src/main/java/cn/histo/provider/service/demo/DemoServiceImpl.java
@@ -3,8 +3,11 @@ package cn.histo.provider.service.demo;
 import cn.histo.provider.api.demo.DemoServiceI;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 @RestController
 @DubboService
 public class DemoServiceImpl implements DemoServiceI {
@@ -12,8 +15,14 @@ public class DemoServiceImpl implements DemoServiceI {
     @Value(value = "${spring.application.name}")
     private String serviceName;
 
+    @Value(value = "${provider.auto-refresh}")
+    private Boolean autoRefresh;
+
+    @Value(value = "${provider.state}")
+    private String state;
+
     @Override
     public String sayHello(String name) {
-        return String.format("[%s] : Hello, %s", serviceName, name);
+        return String.format("[%s] : Hello, %s, the Config is [autoRefresh = %s, state = %s]", serviceName, name, autoRefresh, state);
     }
 }
--- provider-sample/src/main/java/cn/histo/provider/service/demo1/Demo1ServiceImpl.java
+++ provider-sample/src/main/java/cn/histo/provider/service/demo1/Demo1ServiceImpl.java
@@ -3,8 +3,11 @@ package cn.histo.provider.service.demo1;
 import cn.histo.provider.api.demo1.Demo1ServiceI;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 @RestController
 @DubboService
 public class Demo1ServiceImpl implements Demo1ServiceI {
@@ -12,8 +15,14 @@ public class Demo1ServiceImpl implements Demo1ServiceI {
     @Value(value = "${spring.application.name}")
     private String serviceName;
 
+    @Value(value = "${provider.auto-refresh}")
+    private Boolean autoRefresh;
+
+    @Value(value = "${provider.state}")
+    private String state;
+
     @Override
     public String sayBye(String name) {
-        return String.format("[%s] : Bye, %s", serviceName, name);
+        return String.format("[%s] : Bye, %s, the Config is [autoRefresh = %s, state = %s]", serviceName, name, autoRefresh, state);
     }
 }
