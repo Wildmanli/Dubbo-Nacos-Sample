78607aa98439953074a47d2686c0f3c756d12cd8
Wildmanli <wildmanli@hotmail.com>
2021-09-02 19:30:12 +0800

    调整 provider-demo 配置

--- provider-sample/build.sh
+++ provider-sample/build.sh
@@ -1,2 +1,4 @@
-docker rmi provider:${1}
-docker build -f ./Dockerfile -t provider:${1} .
+docker rmi registry.cn-shanghai.aliyuncs.com/histomed/test:${1}
+docker build -t registry.cn-shanghai.aliyuncs.com/histomed/test:${1} .
+docker login --username=docker@histomed registry.cn-shanghai.aliyuncs.com
+docker push registry.cn-shanghai.aliyuncs.com/histomed/test:${1}
